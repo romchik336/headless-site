aa9d96e06fef633df40b5a645ec305b5f5bd23f2
romchik336 <129152601+romchik336@users.noreply.github.com>
2026-05-15 08:10:37 +0000

    фікс бази даних

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # headless-site
+#test
--- app/next.config.ts
+++ app/next.config.ts
@@ -12,6 +12,12 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 const nextConfig: NextConfig = {
+  // Додано блок для дозволу запитів у GitHub Codespaces
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*.github.dev', 'localhost:3000'],
+    },
+  },
   // Temporarily required on Windows until Next.js fixes Turbopack Sass resolution.
   // See: https://github.com/vercel/next.js/issues/86431
   sassOptions: {
